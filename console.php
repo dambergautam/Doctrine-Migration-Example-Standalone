845b4bc49fc1c261e94fe892917ba66e953c3b71
Damber Prasad Gautam <damber.gautam@gmail.com>
2016-11-07 23:25:52 +1000

    Create console.php
    - Load Doctrine package
    - Define database property and set database configuration
    - Add more migration commands
    
    Run(Example):
    $ php console.php list migrations
    $ php console.php --configuration="migrations.yml" migrations:status
    $ php console.php --configuration="migrations.yml" migrations:generate

---
+++ console.php
@@ -0,0 +1,43 @@
+<?php
+$root = '/var/www/html/doctrine-example/';
+require $root.'/vendor/autoload.php';
+
+
+$params= array(
+    'dbname' => 'db_demo',
+    'user' => 'root',
+    'password' => 'demo123',
+    'host' => 'localhost',
+    'driver' => 'mysqli'
+);
+
+
+use Doctrine\DBAL\DriverManager;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Helper\HelperSet;
+
+$db = DriverManager::getConnection($params);
+$helperSet = new HelperSet(array(
+    'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($db),
+    'dialog' => new \Symfony\Component\Console\Helper\QuestionHelper
+));
+
+
+$application = new Application();
+$application->setHelperSet($helperSet);
+
+// ... register commands
+$application->addCommands(array(
+    // ...
+
+    // Migrations Commands
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand()
+));
+
+
+$application->run();
